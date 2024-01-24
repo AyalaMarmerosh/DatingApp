48b7bcf7a3b29d3c1401038f121b7d60d707c0e3
ayalaMarmerosh <am0548505474@gmail.com>
2024-01-24 14:02:41 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,5 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using API;
-
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,12 +11,12 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
