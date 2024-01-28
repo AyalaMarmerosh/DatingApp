e0bb30c808ebbd24d81ec2a922d3511ca34ea24e
ayalaMarmerosh <am0548505474@gmail.com>
2024-01-28 12:22:35 +0200

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,39 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers{
+namespace API.Controllers
+{
     // [Authorize]
     public class UsersController : BaseApiController
-{
-    private readonly DataContext _context;
-    public UsersController(DataContext context)
     {
-        _context = context;
-    }
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await _context.Users.ToListAsync();
-        return users;
-    }
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
 
-    [HttpGet("{id}")] 
-    public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
-        return await _context.Users.FindAsync(id);
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users = await _userRepository.GetMembersAsync();
+            
+            return Ok(users);
+        }
+
+        [HttpGet("{username}")] 
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
+        }
     }
-}
 
 }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+namespace API.Entities
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalcuateAge(this DateOnly dob)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var age = today.Year - dob.Year;
+
+            if (dob > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+
+}
